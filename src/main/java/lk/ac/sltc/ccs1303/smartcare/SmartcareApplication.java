cc71d7b8809a6a937bda3563fcfbfe801cfd664a
orionpear <96986933+orionpear@users.noreply.github.com>
2026-08-19 21:18:59 +0530

    Initial Commit
    This commit contains the most basic boilerplate of the project.

---
+++ src/main/java/lk/ac/sltc/ccs1303/smartcare/SmartcareApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.sltc.ccs1303.smartcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ac/sltc/ccs1303/smartcare/SmartcareApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.sltc.ccs1303.smartcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
